3d8fb6da45cba87c10c5a824228d714177211f3c
quimeymorando <kimi.morando@gmail.com>
2026-02-20 13:28:32 -0300

    feat: redirect root to basePath

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/abundancia-masterclass",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/abundancia-masterclass",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
